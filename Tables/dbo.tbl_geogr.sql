33aeb160d40acf5bef4ebc77f98e2ca6d58ccbf2
Василий Климушев <VasiliyK@Devart.com>
2021-02-18 20:09:58 +0200

    geo

---
+++ Tables/dbo.tbl_geogr.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tbl_geogr] (
+  [id] [int] IDENTITY,
+  [geogr] [geography] NULL,
+  CONSTRAINT [PK_tbl_geogr_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Tables/dbo.tbl_geom.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tbl_geom] (
+  [id] [int] IDENTITY,
+  [c_geom] [geometry] NULL,
+  CONSTRAINT [PK_tbl_geom_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
